790cec713d461e4117967be153eb45428b83e880
ooferi <pilipino115@gmail.com>
2023-10-03 13:23:47 -0500

    add files

--- test.py
+++ test.py
@@ -1,8 +1,19 @@
-from chatterbot import ChatBot
-from chatterbot.trainers import ListTrainer
+import pickle
+#import tensorflow as tf
+#tf.config.list_physical_devices('GPU')
+#file = open('python_how_to_do_it_by_classifier_multiple_iid_to_code.pickle', 'rb')
+text_word_vocab = pickle.load(open('text_word_vocab.pickle', 'rb'))
+code_token_vocab = pickle.load(open('code_token_vocab.pickle', 'rb'))
 
-test = ChatBot(name=Pybot, read_only=True, logic_adapters=['chatterbot.logic.MathematicalEvaluation', 'chatterbot.logic.BestMatch'])
 
-list_trainer = ListTrainer(my_bot)
-for item in (small_talk, math_talk_1, math_talk_2):
-    list_trainer.train(item)
+print('Showing the pickled data:')
+
+cnt = 0
+for item in text_word_vocab:
+    print('The data ', cnt, ' is : ', item)
+    cnt += 1
+
+count = 0
+for item in code_token_vocab:
+    print('The data ', count, ' is : ', item)
+    count += 1
